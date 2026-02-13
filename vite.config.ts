c98bb4a529042743fc220cb3d19e105ef9efa4e4
Carl Olsen <unstoppablecarlolsen@gmail.com>
2026-02-13 15:51:02 -0500

    bump

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/wave-function-collapse-js/',
-
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
   plugins: [vue()],
 })
