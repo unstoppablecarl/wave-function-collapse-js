fda4c802f4d5915c5708f031fa32809f38acd27a
Carl Olsen <unstoppablecarlolsen@gmail.com>
2026-02-15 11:04:25 -0500

    progress

--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -6,9 +6,9 @@ import { EXAMPLE_IMAGES } from '../lib/example-images.ts'
 import { getImgElementImageData, imageDataToUrlImage } from '../lib/ImageData.ts'
 import { useStore } from '../lib/store.ts'
 import { WFC_WORKER_ID, WorkerMsg, type WorkerResponse } from '../lib/WFCOverlappingModelImageData.worker.ts'
+import GeneratorSettings from './GeneratorSettings.vue'
 import ImageFileInput from './ImageFileInput.vue'
 import PixelImg from './PixelImg.vue'
-import SymmetryInput from './SymmetryInput.vue'
 
 type Attempt = {
   encoded: string,
@@ -215,76 +215,17 @@ const images = EXAMPLE_IMAGES
         </div>
         <p>Examples</p>
         <template v-for="image in images" :key="image">
-            <PixelImg
-              :src="image"
-              class="img-target"
-              :scale="scale"
-              @img-click="setImageDataFromElement($event)"
-            />
+          <PixelImg
+            :src="image"
+            class="img-target"
+            :scale="scale"
+            @img-click="setImageDataFromElement($event)"
+          />
         </template>
       </div>
       <div class="col-5">
-        <div class="row input-section">
-          <div class="col-4">
-            <fieldset class="group input-section"
-                      title="In the Wave Function Collapse (WFC) algorithm, N represents the pattern size (or 'kernel size'). It is the dimension of the small squares the algorithm extracts from your input image to use as its 'building blocks.'">
-              <legend>N</legend>
-              <input type="number" min="1" v-model="settings.N" />
-            </fieldset>
-          </div>
-          <div class="col-4">
-            <fieldset class="group input-section"
-                      title="Forces the bottom row of the output to match a specific pattern from the input. -1 will disable ground">
-              <legend>Ground</legend>
-              <input type="number" min="-1" v-model="settings.initialGround" />
-            </fieldset>
-          </div>
 
-          <div class="col-4">
-            <fieldset class="group input-section">
-              <legend>Seed</legend>
-              <input type="number" v-model="settings.seed" />
-            </fieldset>
-          </div>
-        </div>
-
-        <fieldset class="group input-section">
-          <legend>Output Width/Height</legend>
-          <input type="number" v-model="settings.width" />
-          <input type="number" v-model="settings.height" />
-        </fieldset>
-
-        <div class="row input-section periodic">
-          <div class="col-4">
-            <label
-              data-field
-              class="checkbox-label"
-              title="The algorithm treats the input image like a seamless texture"
-            >
-              <input type="checkbox" v-model="settings.periodicInput" /> Periodic Input
-            </label>
-          </div>
-          <div class="col-4">
-            <label
-              data-field
-              class="checkbox-label"
-              title="Outputs a seamless texture"
-            >
-              <input type="checkbox" v-model="settings.periodicOutput" /> Periodic Output
-            </label>
-          </div>
-
-          <div class="col-4">
-            <fieldset class="group input-section">
-              <legend>Tries</legend>
-              <input type="number" v-model="settings.maxTries" />
-            </fieldset>
-          </div>
-        </div>
-
-        <div class="input-section">
-          <SymmetryInput v-model="settings.symmetry" />
-        </div>
+        <GeneratorSettings />
 
         <div class="hstack">
           <ImageFileInput @imageDataLoaded="setImageDataFromFileInput" />
@@ -372,9 +313,6 @@ const images = EXAMPLE_IMAGES
   </article>
 </template>
 <style lang="scss">
-.input-section {
-  margin-bottom: 1rem;
-}
 
 .pixel-img,
 .canvas-output {
---
+++ src/components/GeneratorSettings.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useStore } from '../lib/store.ts'
+import SymmetryInput from './SymmetryInput.vue'
+
+const store = useStore()
+const { settings } = storeToRefs(store)
+</script>
+
+<template>
+  <div class="row input-section">
+    <div class="col-4">
+      <fieldset class="group input-section"
+                title="In the Wave Function Collapse (WFC) algorithm, N represents the pattern size (or 'kernel size'). It is the dimension of the small squares the algorithm extracts from your input image to use as its 'building blocks.'">
+        <legend>N</legend>
+        <input type="number" min="1" v-model="settings.N" />
+      </fieldset>
+    </div>
+    <div class="col-4">
+      <fieldset class="group input-section"
+                title="Forces the bottom row of the output to match a specific pattern from the input. -1 will disable ground">
+        <legend>Ground</legend>
+        <input type="number" min="-1" v-model="settings.initialGround" />
+      </fieldset>
+    </div>
+
+    <div class="col-4">
+      <fieldset class="group input-section">
+        <legend>Seed</legend>
+        <input type="number" v-model="settings.seed" />
+      </fieldset>
+    </div>
+  </div>
+
+  <fieldset class="group input-section">
+    <legend>Output Width/Height</legend>
+    <input type="number" v-model="settings.width" />
+    <input type="number" v-model="settings.height" />
+  </fieldset>
+
+  <div class="row input-section periodic">
+    <div class="col-4">
+      <label
+        data-field
+        class="checkbox-label"
+        title="The algorithm treats the input image like a seamless texture"
+      >
+        <input type="checkbox" v-model="settings.periodicInput" /> Periodic Input
+      </label>
+    </div>
+    <div class="col-4">
+      <label
+        data-field
+        class="checkbox-label"
+        title="Outputs a seamless texture"
+      >
+        <input type="checkbox" v-model="settings.periodicOutput" /> Periodic Output
+      </label>
+    </div>
+
+    <div class="col-4">
+      <fieldset class="group input-section">
+        <legend>Tries</legend>
+        <input type="number" v-model="settings.maxTries" />
+      </fieldset>
+    </div>
+  </div>
+
+  <div class="input-section">
+    <SymmetryInput v-model="settings.symmetry" />
+  </div>
+</template>
+<style>
+
+</style>
