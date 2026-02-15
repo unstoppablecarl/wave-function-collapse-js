02e5f46c573a92403470efcf2ecf9ecf6cf81dbe
Carl Olsen <unstoppablecarlolsen@gmail.com>
2026-02-15 09:35:56 -0500

    progress

--- src/lib/example-images.ts
+++ src/lib/example-images.ts
@@ -1,17 +1,5 @@
-import ex1 from '../assets/ex1.png'
-import ex2 from '../assets/ex2.png'
-import ex3 from '../assets/ex3.png'
-import ex4 from '../assets/ex4.png'
-import ex5 from '../assets/ex5.png'
-import ex6 from '../assets/ex6.png'
-import flowersPng from '../assets/flowers.png'
+const imageModules = import.meta.glob('../assets/*.png', { eager: true })
 
-export const EXAMPLE_IMAGES = [
-  flowersPng,
-  ex1,
-  ex2,
-  ex3,
-  ex4,
-  ex5,
-  ex6,
-]
+export const EXAMPLE_IMAGES = Object.values(imageModules).map(
+  (module) => (module as any).default,
+)
