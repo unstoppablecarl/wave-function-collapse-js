98b58e9ae8ef1d85a438092d8da1474153f79b26
Carl Olsen <unstoppablecarlolsen@gmail.com>
2026-02-22 13:13:09 -0500

    rust wasm

--- vite.config.ts
+++ vite.config.ts
@@ -15,8 +15,7 @@ export default defineConfig({
     topLevelAwait()
   ],
   worker: {
-    // Not needed with vite-plugin-top-level-await >= 1.3.0
-    // format: "es",
+    format: "es",
     plugins: () => [
       wasm(),
       topLevelAwait()
